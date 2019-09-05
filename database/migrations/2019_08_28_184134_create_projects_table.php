4a7d219f0b5a25679b8e51e63aeef22906517b48
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2019-09-05 08:18:35 +0200

    Using bigIncrements to support a potentially large number of projects.

--- database/migrations/2019_08_28_184134_create_projects_table.php
+++ database/migrations/2019_08_28_184134_create_projects_table.php
@@ -14,7 +14,7 @@ class CreateProjectsTable extends Migration
     public function up()
     {
         Schema::create('projects', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->unsignedBigInteger('team_id');
             $table->foreign('team_id')->references('id')->on('teams');
             $table->unsignedBigInteger('environment_id');
